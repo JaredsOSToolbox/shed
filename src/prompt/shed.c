b5186c71c766d51df2ac06989923989207d4cc0a
JaredDyreson <jared.dyreson@gmail.com>
2021-02-20 23:35:41 -0800

    this iteration works

--- src/prompt/shed.c
+++ src/prompt/shed.c
@@ -27,32 +27,66 @@ int main(void) {
     int running = 1;
 
     struct history* history = history_constructor();
+    struct command_t** garbage = (struct command_t**)malloc(MAX_WORDS * sizeof(struct command_t*));
+    int j = 0;
 
-    while(running){
-        printf("shed > ");
-        fflush(stdout);
+    for(int i = 0; i < 2; ++i){
+
         char* line =  get_line();
-        if(line == NULL){ continue; }
-
-        if(strcmp(line, "!!") == 0){
-            // find prev command
-            if(history_isempty(history)) {
-                fprintf(stderr, "could not find previous command; history empty\n");
-                continue;
-            } else{
-                line = history_previous(history);
-                printf("previous %s\n", line);
-            }
-        } else {
-            history_insert(history, line);
-        }
-        if(strcmp(line, "q") == 0) { break; }
-        printf("using %s as the command\n", line);
+        printf("got %s\n", line);
+        history_insert(history, line);
         struct command_t* command = command_t_constructor(line);
-        command_t_invoke(command);
-        command_t_destructor(command);
-        /*running = 0;*/
+        command_t_print(command);
+
+        pid_t child;
+        int wstatus;
+        child = spawnChild(command->command_path, command->arguments);
+        if(waitpid(child, &wstatus, WUNTRACED | WCONTINUED) == -1){
+            perror("waitpid");
+            exit(EXIT_FAILURE);
+        }
+        garbage[j++] = command;
+
+        /*command_t_destructor(command);*/
+        /*free(line);*/
     }
+    /*
+     * for some reason, invoking the destructor in the while/for loop
+     * there is an overflow bug in the following iterations (after 1st)
+     * but it seems to be fixed when doing a garbage collection at the very end
+     * why this works, i have no idea. I truly don't.
+    */
+
+    for(int k = 0; k < j; ++k){
+        command_t_destructor(garbage[k]);
+    }
+    free(garbage);
+
+    /*while(running){*/
+        /*printf("shed > ");*/
+        /*fflush(stdout);*/
+        /*char* line =  get_line();*/
+        /*if(line == NULL){ continue; }*/
+
+        /*if(strcmp(line, "!!") == 0){*/
+            /*// find prev command*/
+            /*if(history_isempty(history)) {*/
+                /*fprintf(stderr, "could not find previous command; history empty\n");*/
+                /*continue;*/
+            /*} else{*/
+                /*line = history_previous(history);*/
+                /*printf("previous %s\n", line);*/
+            /*}*/
+        /*} else {*/
+            /*history_insert(history, line);*/
+        /*}*/
+        /*if(strcmp(line, "q") == 0) { break; }*/
+        /*printf("using %s as the command\n", line);*/
+        /*struct command_t* command = command_t_constructor(line);*/
+        /*command_t_invoke(command);*/
+        /*command_t_destructor(command);*/
+        /*[>running = 0;<]*/
+    /*}*/
 
     printf("\n");
     history_print(history);
--- src/prompt/src/command_t.c
+++ src/prompt/src/command_t.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/wait.h>
 
 struct command_t* command_t_constructor(char* line) {
@@ -11,62 +12,87 @@ struct command_t* command_t_constructor(char* line) {
     char* command_path = (char*)malloc(MAX_COMMAND_NAME_SIZ * sizeof(char));
     char** command_args = malloc((MAX_WORDS + 1) * sizeof(char*));
 
-    for(int j = 0; j < MAX_WORDS; ++j){
-        command_args[j] = malloc((MAX_COMMAND_NAME_SIZ+1) * sizeof(char));
-    }
-
     int i = 0;
 
     const char delimiter[2] = " ";
     char* token; 
 
     token = strtok(line, delimiter);
-    while(token != NULL) {
+    while((token != NULL)  && ((i - 1) < MAX_WORDS)) {
         if(i == 0){
             strncpy(command_path, token, strlen(token));
-            strncpy(command_args[i], token, strlen(token));
-        } else{
-            strncpy(command_args[i], token, strlen(token));
-        }
+            printf("command path yields: %s\n", command_path);
+        } 
+        command_args[i] = malloc((MAX_COMMAND_NAME_SIZ + 1) * sizeof(char));
+        strncpy(command_args[i], token, strlen(token));
         token = strtok(NULL, delimiter);
         ++i;
     }
 
     command->command_path = command_path;
     command->arguments = command_args;
-    command->argc = i-1;
+    command->argc = i;
 
-    // trim off excess data
-    // bad design, can be fixed
-    /*for(int j = i; j < MAX_WORDS; ++j){ free(command->arguments[j]); }*/
     return command;
 }
 
 void command_t_destructor(struct command_t* command){
     free(command->command_path);
 
-    for(int i = 0; i < MAX_WORDS; ++i){ free(command->arguments[i]); }
+    for(int i = 0; i < command->argc; ++i){ free(command->arguments[i]); }
 
     free(command->arguments);
     free(command);
 }
 
-void command_t_invoke(struct command_t* command) {
-    pid_t pid = fork();
-    if(pid < 0){
-        fprintf(stderr, "fork failed, use a spoon\n");
-        return;
+pid_t spawnChild(char* program, char** arg_list) {
+    pid_t ch_pid = fork();
+    if (ch_pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
     }
-    else if(pid == 0){
-        int status = execvp(command->command_path, command->arguments);
-        if(status == EOF){
-            fprintf(stderr, "process cowardly refused to terminate\n");
-            exit(1);
-        }
-    } else{
-        wait(NULL);
+
+    if (ch_pid > 0) {
+        return ch_pid;
+    } else {
+        execvp(program, arg_list);
+        perror("execve");
+        exit(EXIT_FAILURE);
     }
 }
+
+void command_t_invoke(struct command_t* command) {
+    pid_t child;
+    int wstatus;
+    child = spawnChild(command->command_path, command->arguments);
+    if(waitpid(child, &wstatus, WUNTRACED | WCONTINUED) == -1){
+        perror("waitpid");
+        exit(EXIT_FAILURE);
+    }
+
+}
+
+/*void command_t_invoke(struct command_t* command) {*/
+    /*pid_t pid = fork();*/
+    /*int wstatus;*/
+    /*if(pid < 0){*/
+        /*fprintf(stderr, "fork failed, use a spoon\n");*/
+        /*return;*/
+    /*}*/
+    /*else if(pid == 0){*/
+        /*int status = execvp(command->command_path, command->arguments);*/
+        /*if(status == -1) {*/
+            /*fprintf(stderr, "process cowardly refused to terminate\n");*/
+            /*exit(1);*/
+        /*}*/
+    /*} else {*/
+        /*printf("waiting for pid to fininsh\n");*/
+        /*if(waitpid(pid, &wstatus, WUNTRACED | WCONTINUED) == EOF){*/
+            /*printf("waitpid err\n");*/
+            /*exit(1);*/
+        /*}*/
+    /*}*/
+/*}*/
 
 void command_t_print(struct command_t* command) {
     printf("command_path: %s\n", command->command_path);
