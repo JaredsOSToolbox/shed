275eaa8a6a12ae142afb975338802edfbe78c6f7
JaredDyreson <jared.dyreson@gmail.com>
2021-03-02 01:49:23 -0800

    strays

--- src/prompt/shed_unit.c
+++ src/prompt/shed_unit.c
@@ -12,6 +12,13 @@
 int main(int argc, const char* argv[]) {
 
     struct command_t** commands = parse_line("echo line | grep -o l");
+    int i = 0, z = 0;
+
+    while(commands[z] != NULL) {
+        if(commands[z]->pipe_stream && (z + 1 < COM_SIZ)){
+            command_t_set_pipe_stream(commands[z])
+        }
+    }
 
     return 0;
 }
