a99cf3941b7924b64226431d3bbb53039389373a
JaredDyreson <jared.dyreson@gmail.com>
2021-03-13 14:45:21 -0800

    pipe support

--- src/prompt/shed.c
+++ src/prompt/shed.c
@@ -39,9 +39,6 @@ void run_pipeline(struct command_t** container) {
             exit(1);
         } else if(pid == 0){
             dup2(fdd, 0);
-            /*if(*(container + 1) != NULL){*/
-                /*dup2(fd[1], 1);*/
-            /*}*/
             if(container[z+1] != NULL){
                 dup2(fd[1], 1);
             }
@@ -114,14 +111,20 @@ int main(int argc, const char* argv[]) {
         while(commands[z] != NULL && garbage_position < MAX_FILTH && pipe_line_position < MAX_PIPELINE_LEN) {
             if(commands[z]->pipe_stream == 1 && (z + 1 < COM_SIZ)) {
                 // add to pipeline for processing
-                command_t_print(commands[z]);
+                /*command_t_print(commands[z]);*/
                 garbage[garbage_position++] = commands[z];
                 pipeline[pipe_line_position++] = commands[z];
             } 
             else if(commands[z]->output_stream){
                 stdout_old = dup(1);
                 command_t_set_output_stream(commands[z+1]->stream_path);
-                command_t_invoke(commands[z]);
+                if(commands[z+2] == NULL && pipe_line_position > 0) {
+                    pipeline[pipe_line_position++] = commands[z];
+                    run_pipeline(pipeline);
+                    /*break;*/
+                } else {
+                    command_t_invoke(commands[z]);
+                }
                 flag_t_set_flag(fl, OUTPUT);
             } 
             else if(commands[z]->input_stream) {
@@ -135,10 +138,10 @@ int main(int argc, const char* argv[]) {
             ++z;
         }
 
-        if(pipe_line_position > 0){
-            run_pipeline(pipeline);
+        /*if(pipe_line_position > 0){*/
+            /*run_pipeline(pipeline);*/
         
-        }
+        /*}*/
         // reset output
         // https://stackoverflow.com/questions/11042218/c-restore-stdout-to-terminal
         restore_stdout(stdout_old);
