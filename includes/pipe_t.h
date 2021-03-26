369ba3d520671685aa0e8aac7fc37e7b460821f9
JaredDyreson <jared.dyreson@gmail.com>
2021-03-25 22:02:04 -0700

    removed pipe_t struct

--- includes/pipe_t.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-struct pipe_t {
-  int* pipefds;  // pipe file descriptors
-  int size;      // amount of file descriptors
-  int n;         // current index
-};
-
-struct pipe_t* pipe_t_constructor(int);
-void pipe_t_clear(pipe_t*);
